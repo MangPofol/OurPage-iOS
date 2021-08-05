7ab87db1b4c872fcf1669bb1ccc4a60bd3fd6800
LeeNamJun <dlskawns96@gmail.com>
2021-08-05 12:23:14 +0900

    Chore: 추가 iOS 프로젝트 폴더, dependency
    iOS 프로젝트 폴더 생성 - Deployment Target: iOS 13.0 / iPhone
    Dependency 추가 - Snapkit, Then, Moya, RxSwift

---
+++ BookClub_iOS/BookClub_iOS/ViewControllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BookClub_iOS
+//
+//  Created by Lee Nam Jun on 2021/08/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .red
+    }
+
+
+}
+
